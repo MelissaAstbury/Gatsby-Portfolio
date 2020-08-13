6ed7ac57212f01f193187cfaf4ccb208466f85b6
Melissa Astbury <melissajastbury@gmail.com>
2020-08-13 09:46:14 +0100

    rendered an image by using gatsby-image and graphQL. This is now displayed in the Hero file showing on the main page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,32 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+const query = graphql`
+  {
+    file(relativePath: { eq: "portfolio-image.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info"></article>
+        <Image fluid={fluid} />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
